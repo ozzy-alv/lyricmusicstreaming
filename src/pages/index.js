83cc2a2b7dd21dd252d5405688755d1782c0127f
Ozzy <ozzyalvarado96@gmail.com>
2022-11-29 20:14:37 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Discover from './Discover';
+
+import ArtistDetails from './ArtistDetails';
+import SongDetails from './SongDetails';
+import Search from './Search';
+import TopCharts from './TopCharts';
+
+export { Discover, Search, ArtistDetails, SongDetails, TopCharts };
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,49 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '8a3c07a361msh7fb9540e377890ap1f867cjsne9cc3eef5680',
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        // eslint-disable-next-line implicit-arrow-linebreak
+        `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `/artists/details?artist_id=${artistId}`,
+    }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `/tracks/related?track_id=${songid}`,
+    }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreApi;
